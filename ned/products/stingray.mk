162783187c96e8b4e458eb255154bd930e5a19cd
Ned <iamcalledned@gmail.com>
2012-01-20 15:41:34 -0500

    updated stingray build fingerprint

--- ned/products/stingray.mk
+++ ned/products/stingray.mk
@@ -17,7 +17,10 @@ PRODUCT_DEVICE := stingray
 PRODUCT_MODEL := Moto Xoom
 PRODUCT_MANUFACTURER := motorola
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=full_stingray BUILD_ID=ICL53F BUILD_FINGERPRINT=verizon/trygon/stingray:3.2.2/HLK75D/200096:user/release-keys PRIVATE_BUILD_DESC="trygon-user 3.2.2 HLK75D 200096 release-keys" BUILD_NUMBER=200096
+#PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=full_stingray BUILD_ID=ICL53F BUILD_FINGERPRINT=verizon/trygon/stingray:3.2.2/HLK75D/200096:user/release-keys #PRIVATE_BUILD_DESC="trygon-user 3.2.2 HLK75D 200096 release-keys" BUILD_NUMBER=200096
+
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=trygon BUILD_ID=HLK75F BUILD_FINGERPRINT=verizon/trygon/stingray:3.2.4/HLK75F/221360:user/release-keys PRIVATE_BUILD_DESC="trygon-user 3.2.4 HLK75F 221360 release-keys" BUILD_NUMBER=221360
+
 
 # Inherit common build.prop overrides
 -include vendor/ned/products/common_versions.mk
