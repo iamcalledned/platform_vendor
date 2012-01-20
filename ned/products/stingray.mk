58dc1d3194720324c545249f49567eed851b770c
Ned <iamcalledned@gmail.com>
2012-01-20 16:00:20 -0500

    updated model

--- ned/products/stingray.mk
+++ ned/products/stingray.mk
@@ -14,8 +14,8 @@ $(call inherit-product, vendor/moto/stingray/stingray-vendor.mk)
 PRODUCT_NAME := ned_stingray
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := stingray
-PRODUCT_MODEL := Moto Xoom
-PRODUCT_MANUFACTURER := motorola
+PRODUCT_MODEL := Xoom
+PRODUCT_MANUFACTURER := Motorola
 
 #PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=full_stingray BUILD_ID=ICL53F BUILD_FINGERPRINT=verizon/trygon/stingray:3.2.2/HLK75D/200096:user/release-keys #PRIVATE_BUILD_DESC="trygon-user 3.2.2 HLK75D 200096 release-keys" BUILD_NUMBER=200096
 
