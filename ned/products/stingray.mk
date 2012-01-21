a1ddad5996dc379e96b12dabed39a914c9cd32eb
Ned <iamcalledned@gmail.com>
2012-01-21 14:55:22 -0500

    bug fix

--- ned/products/stingray.mk
+++ ned/products/stingray.mk
@@ -46,12 +46,12 @@ PRODUCT_COPY_FILES +=  \
     vendor/ned/proprietary/common/lib/libvideochat_jni.so:system/lib/libvideochat_jni.so \
     vendor/ned/proprietary/common/lib/libvideochat_stabilize.so:system/lib/libvideochat_stabilize.so \
     vendor/ned/proprietary/common/lib/libvoicesearch.so:system/lib/libvoicesearch.so \
-    vendor/pete/proprietary/stingray/app/StingrayQuickOffice.apk:system/app/StingrayQuickOffice.apk \
-    vendor/pete/proprietary/common/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
-    vendor/pete/proprietary/common/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
-    vendor/pete/proprietary/common/lib/libfilterpack_facedetect.so:system/lib/libfilterpack_facedetect.so \
-    vendor/pete/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/pete/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+    vendor/ned/proprietary/common/app/StingrayQuickOffice.apk:system/app/StingrayQuickOffice.apk \
+    vendor/ned/proprietary/common/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
+    vendor/ned/proprietary/common/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
+    vendor/ned/proprietary/common/lib/libfilterpack_facedetect.so:system/lib/libfilterpack_facedetect.so \
+    vendor/ned/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/ned/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
 
 # Inherit common build.prop overrides
