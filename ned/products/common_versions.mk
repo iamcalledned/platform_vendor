fadd2c8ab8d829646f6c3e688876e272a2a8da16
Ned <iamcalledned@gmail.com>
2012-01-23 13:30:56 -0500

    updated to mr5

--- ned/products/common_versions.mk
+++ ned/products/common_versions.mk
@@ -1,4 +1,4 @@
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K-MR4 BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K-MR5 BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 
